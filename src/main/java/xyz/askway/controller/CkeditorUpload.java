1626fcd335af135831fe1a2f539715061e0112b3
Uncle-LiuY <nanfonjin@foxmail.com>
2018-10-30 15:31:42 +0800

    2018-10-30 15:31

--- src/main/java/xyz/askway/controller/CkeditorUpload.java
+++ src/main/java/xyz/askway/controller/CkeditorUpload.java
@@ -1,30 +1,18 @@
-package xyz.askway.ly.controller;
+package xyz.askway.controller;
 
 
-import java.io.File;  
-import java.io.FileInputStream;  
-import java.io.FileOutputStream;  
-import java.io.InputStream;  
-import java.io.OutputStream;  
-import java.io.PrintWriter;
-
-import javax.annotation.Resource;
-import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 import xyz.askway.util.ImageUploadUtil;
 
 /** 
  * @ClassName: CkeditorUpload 
- * @Description: TODO() 
+ * @Description: TODO(ckeditor图片上传)
  * @author Uncle
  *  
  */
